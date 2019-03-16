67810f0a9dda6c8ad4978a01512df0ae8b2b7536
giribroba <giribroba@gmail.com>
2019-03-16 18:24:15 -0300

    Movimento e limitaçao de movimento adicionados

--- Assets/Scripts/playerBehavior.cs
+++ Assets/Scripts/playerBehavior.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class playerBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed;
+    public float xMax;
+    public float yMax;
 
-    // Update is called once per frame
+    float xMoviment;
+    float yMoviment;
+
+
     void Update()
     {
+        Moviment();
+        Limit();
+    }
+
+    /// <summary>
+    /// Essa função contraa a movimentação do personagem
+    /// </summary>
+    void Moviment()
+    {
+        //Impulso
+        xMoviment = Input.GetAxis("Horizontal");
+        yMoviment = Input.GetAxis("Vertical");
         
+        //Direção
+        transform.Translate(Vector3.right * xMoviment * speed * Time.deltaTime);
+        transform.Translate(Vector3.up * yMoviment * speed * Time.deltaTime);
+    }
+
+    /// <summary>
+    /// Essa função limita o movimento do player
+    /// </summary>
+    void Limit()
+    {
+        //Eixo X
+        if (transform.position.x > xMax)
+        {
+            transform.position = new Vector3(xMax, transform.position.y);
+        }
+        else if (transform.position.x < -xMax)
+        {
+            transform.position = new Vector3(-xMax, transform.position.y);
+        }
+
+        //Eixo Y
+        if (transform.position.y > yMax)
+        {
+            transform.position = new Vector3(transform.position.x, yMax);
+        }
+        else if (transform.position.y < -yMax)
+        {
+            transform.position = new Vector3(transform.position.x, -yMax);
+        }
     }
 }
