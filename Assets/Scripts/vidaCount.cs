dc12c49aec7ae4b96a195f8a3eb8992729cc1ee0
GD2032 <gabrieldias2032@gmail.com>
2019-03-17 01:54:59 -0300

    Mudanças no vida count

--- Assets/Scripts/vidaCount.cs
+++ Assets/Scripts/vidaCount.cs
@@ -14,7 +14,14 @@ public class vidaCount : MonoBehaviour
 
     void Update()
     {
-        //Altera o tamanho da barra de acordo com a vida atual
+        BarraVida();
+       
+    }
+    /// <summary>
+    /// Altera o tamanho da barra de acordo com a vida atual
+    /// </summary>
+    void BarraVida()
+    {
         if (transform.localScale.x < Vida / 20 && Vida >= 0)
         {
             transform.localScale = new Vector3(transform.localScale.x + 0.2f, transform.localScale.y, transform.localScale.z);
