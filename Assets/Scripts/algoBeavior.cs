f208e873a4ec03554eb59a74f797414a1f83d302
giribroba <giribroba@gmail.com>
2019-03-17 02:22:36 -0300

    Bug ricochete

--- Assets/Scripts/algoBeavior.cs
+++ Assets/Scripts/algoBeavior.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class algoBeavior : MonoBehaviour
 {
     float empurrando;
+    float xPush;
+    float yPush;
 
     public float speed;
 
@@ -18,6 +20,8 @@ public class algoBeavior : MonoBehaviour
         {   
             vidaCount.Vida -= 10;
             empurrando = tempoEmpurrar;
+            xPush = playerBehavior.xMoviment;
+            yPush = playerBehavior.yMoviment;
         }
     }
 
@@ -26,14 +30,31 @@ public class algoBeavior : MonoBehaviour
         //Empurra o player ao colidir com o objeto
         if (empurrando > 0 && (playerBehavior.xMoviment != 0 || playerBehavior.yMoviment != 0))
         {
-            Player.transform.Translate(Vector3.up * -playerBehavior.yMoviment * speed * Time.deltaTime);
-            Player.transform.Translate(Vector3.right * -playerBehavior.xMoviment * speed * Time.deltaTime);
             playerBehavior.move = false;
+            Player.transform.Translate(Vector3.up * -yPush * speed * Time.deltaTime);
+            Player.transform.Translate(Vector3.right * -xPush * speed * Time.deltaTime);
         }
         else
         {
             playerBehavior.move = true;
         }
         empurrando -= Time.deltaTime;
+        if (xPush > 0)
+        {
+            xPush -= 0.7f * Time.deltaTime;
+        }
+        else if (xPush < 0)
+        {
+            xPush += 0.7f * Time.deltaTime;
+        }
+        if (yPush > 0)
+        {
+            yPush -= 0.7f * Time.deltaTime;
+        }
+        else if (yPush < 0)
+        {
+            yPush += 0.7f * Time.deltaTime;
+        }
     }
+
 }
